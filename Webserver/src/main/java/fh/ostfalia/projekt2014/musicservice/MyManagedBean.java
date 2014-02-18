02afbf01334caa23f581d49c283ae251c43f0624
MalteToenjes <Mettbrtchen@Mettbroetchen.fritz.box>
2014-02-18 17:47:34 +0100

    RMI testen

--- Webserver/src/main/java/fh/ostfalia/projekt2014/musicservice/MyManagedBean.java
+++ Webserver/src/main/java/fh/ostfalia/projekt2014/musicservice/MyManagedBean.java
@@ -15,22 +15,30 @@ import javax.ejb.EJB;
  *
  * @author Yannick
  */
-@ManagedBean
+
 public class MyManagedBean extends RemoteManagedBean{
     /**
      * Creates a new instance of MyManagedBean
      */
     public MyManagedBean() {
-        super("localhost", "1099", "java:global/Musicservice");
+        super("localhost", "3700", "java:global/fh.ostfalia.projekt2014.musikdienst/MyFirstBean");
+    }
+    
+    public void getString(){
+        System.out.println("StartTest....");
+        MyFirstBeanRemote obj = (MyFirstBeanRemote) super.getObject();
+        System.out.println("TEST " + obj.getString());
     }
     
     @PostConstruct
     public void init(){
-        MyFirstBeanRemote obj = (MyFirstBeanRemote) super.getObject();
-        System.out.println("TTTEESSST: "+ obj.getValue());
+        //MyFirstBeanRemote obj = (MyFirstBeanRemote) super.getObject();
+        //System.out.println("TTTEESSST: "+ obj.getValue());
         //MyFirstBeanRemote object = (MyFirstBeanRemot) super.getRemoteObject();
         //cityList = new ArrayList<String> (object.getCitylist());    //hole alle Städte (Stadtplan)
         //Collections.sort(cityList);                                 //sortieren
     }
+    
+
     
 }
