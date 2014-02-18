4d0d784871c2af50eb550c7b9d829101d2b0bb45
Yannick <Yannick@YannickRechner>
2014-02-18 18:55:09 +0100

    Remotezugriff von Webserver nach Musikdienst funktioniert

---
+++ Webserver/src/main/java/fh/ostfalia/projekt2014/musicservice/Mp3DaoMBean.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fh.ostfalia.projekt2014.musicservice;
+
+/**
+ *
+ * @author Yannick
+ */
+public class Mp3DaoMBean {
+
+    /**
+     * Creates a new instance of Mp3DaoMBean
+     */
+    public Mp3DaoMBean() {
+    }
+}
