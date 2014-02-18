93d52d574f206cef60ce7f1b454672a680fa2087
Yannick <Yannick@YannickRechner>
2014-02-18 15:01:54 +0100

    Loadbalancer, Musikdienst, Kommentardienst als EJBs angelegt, Webserver ist jetzt die Präsentationsschicht

---
+++ MusicServiceInterface/src/main/java/ejb/MyFirstRemoteBeanRemote.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author Yannick
+ */
+@Remote
+public interface MyFirstRemoteBeanRemote {
+    public int getValue();
+}
---
+++ MusicServiceInterface/src/main/java/fh/ostfalia/projekt2014/musicserviceinterface/MusicServiceInterface.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fh.ostfalia.projekt2014.musicserviceinterface;
+
+/**
+ *
+ * @author Yannick
+ */
+public interface MusicServiceInterface {
+    
+}
