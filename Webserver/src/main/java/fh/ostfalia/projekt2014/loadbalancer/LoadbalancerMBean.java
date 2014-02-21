e60a20fe05313633bd2c748aa981cd7014b1c537
davidfahr <david.fahr@gmx.net>
2014-02-21 11:52:34 +0100

    LoadBalancer Anfang

---
+++ Webserver/src/main/java/fh/ostfalia/projekt2014/loadbalancer/LoadbalancerMBean.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fh.ostfalia.projekt2014.loadbalancer;
+
+import fh.ostfalia.projekt2014.beanmanager.RemoteManagedBean;
+import fh.ostfalia.projekt2014.loadbalancerremoteinterfaces.entities.LoadbalancerResult;
+import fh.ostfalia.projekt2014.loadbalancerremoteinterfaces.interfaces.Loadbalancer;
+import javax.annotation.PostConstruct;
+
+/**
+ *
+ * @author Yannick
+ */
+public class LoadbalancerMBean extends RemoteManagedBean{
+    private Loadbalancer loadbalancerBean;
+    private LoadbalancerResult loadbalancerResult;
+    
+    public LoadbalancerMBean() {
+         super("localhost", "3700", "java:global/Loadbalancer/LoadbalancerBean");
+    }
+    
+     @PostConstruct
+    public void initBean() {
+        //Holen der entfernten LoadbalancerBean bzw. deren Stub-Objekt
+       loadbalancerBean = (Loadbalancer) super.getObject();
+    }
+    
+    public void startLoadbalancerSimulation(){
+        this.loadbalancerResult = loadbalancerBean.startLoadbalancerSimulation();
+        loadbalancerResult.getServerId();
+    }
+}
