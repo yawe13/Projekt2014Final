09c68acd17440175e26c47dab49cfe8eae285996
Yannick <Yannick@YannickRechner>
2014-02-22 19:11:16 +0100

    mal wieder neue fehler

--- Loginservice/target/generated-sources/annotations/fh/ostfalia/projekt2014/loginservice/entities/User_.java
+++ Loginservice/target/generated-sources/annotations/fh/ostfalia/projekt2014/loginservice/entities/User_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2014-02-21T14:20:58")
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2014-02-22T18:52:02")
 @StaticMetamodel(User.class)
 public class User_ { 
 
