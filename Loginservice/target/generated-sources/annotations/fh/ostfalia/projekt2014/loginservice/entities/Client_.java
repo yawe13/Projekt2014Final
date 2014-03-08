6e6ebbb26778916c8f9a444958fb99096e91cd80
Mettbroetchen <Mettbroetchen@MettbroetchenPC.fritz.box>
2014-03-08 18:23:39 +0100

    Upload Funktioniert

--- Loginservice/target/generated-sources/annotations/fh/ostfalia/projekt2014/loginservice/entities/Client_.java
+++ Loginservice/target/generated-sources/annotations/fh/ostfalia/projekt2014/loginservice/entities/Client_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2014-03-07T18:36:00")
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2014-03-08T18:13:47")
 @StaticMetamodel(Client.class)
 public class Client_ { 
 
