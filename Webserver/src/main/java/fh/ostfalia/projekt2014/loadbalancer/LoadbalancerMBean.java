093eadad0b3f8d7416929581fc1a134de67c2698
Yannick <Yannick@YannickRechner>
2014-03-09 13:09:48 +0100

    lb fortgesetzt

--- Webserver/src/main/java/fh/ostfalia/projekt2014/loadbalancer/LoadbalancerMBean.java
+++ Webserver/src/main/java/fh/ostfalia/projekt2014/loadbalancer/LoadbalancerMBean.java
@@ -7,7 +7,9 @@ package fh.ostfalia.projekt2014.loadbalancer;
 import fh.ostfalia.projekt2014.beanmanager.RemoteManagedBean;
 import fh.ostfalia.projekt2014.loadbalancerremoteinterfaces.entities.LoadbalancerResult;
 import fh.ostfalia.projekt2014.loadbalancerremoteinterfaces.interfaces.Loadbalancer;
+import java.util.concurrent.Future;
 import javax.annotation.PostConstruct;
+import javax.faces.event.ValueChangeEvent;
 
 /**
  *
@@ -16,19 +18,57 @@ import javax.annotation.PostConstruct;
 public class LoadbalancerMBean extends RemoteManagedBean{
     private Loadbalancer loadbalancerBean;
     private LoadbalancerResult loadbalancerResult;
+    private Future asyncResult;
+    //Variable die zwischen manuellem starten und stoppen des LB oder per definiertem Zeitintervall unterscheidet 
+    //möglich sind manuel oder timed
+    private String mode;
     
     public LoadbalancerMBean() {
-         super("localhost", "3700", "java:global/Loadbalancer/LoadbalancerBean");
+         super("localhost", "3700", "java:global/Webserver/LoadbalancerBean");
     }
+   
     
      @PostConstruct
     public void initBean() {
         //Holen der entfernten LoadbalancerBean bzw. deren Stub-Objekt
        loadbalancerBean = (Loadbalancer) super.getObject();
     }
-    
-    public void startLoadbalancerSimulation(){
-//        this.loadbalancerResult = loadbalancerBean.startLoadbalancerSimulation();
-        loadbalancerResult.getServerId();
+    public String getModePage(){
+        System.out.println(mode);
+        if(mode.equals("manual")){
+            
+            return "manual";
+        }
+        else if(mode.equals("timed")){
+            return "timed";
+        }
+        else{
+            System.out.println("wrong Page!");
+            return null;
+        }
+
     }
+    public void startLoadbalancerSimulation(){
+        asyncResult = loadbalancerBean.startLoadbalancerSimulation();
+        
+    }
+    
+    public void stopLoadbalancerSimulation(){
+        asyncResult.cancel(true);
+       
+    }
+     
+     public void startLoadbalancerSimulationByTime(int seconds){
+        
+    }
+    
+      public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+    
 }
+
