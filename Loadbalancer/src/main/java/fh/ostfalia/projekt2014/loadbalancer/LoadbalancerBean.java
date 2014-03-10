80354044e16ae4debb6eb128fcd51d7b291615b7
Yannick <Yannick@YannickRechner>
2014-03-10 11:31:29 +0100

    Maltes Update zum Musikdienst + LB-Zeitgesteuert

--- Loadbalancer/src/main/java/fh/ostfalia/projekt2014/loadbalancer/LoadbalancerBean.java
+++ Loadbalancer/src/main/java/fh/ostfalia/projekt2014/loadbalancer/LoadbalancerBean.java
@@ -11,73 +11,121 @@ import fh.ostfalia.projekt2014.loadbalancerremoteinterfaces.entities.Loadbalance
 import fh.ostfalia.projekt2014.loadbalancerremoteinterfaces.interfaces.Loadbalancer;
 import fh.ostfalia.projekt2014.musicserviceremoteinterface.interfaces.Musicservice;
 import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+import javax.jms.Queue;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.ejb.ActivationConfigProperty;
 import javax.ejb.AsyncResult;
 import javax.ejb.Asynchronous;
 import javax.ejb.EJB;
+import javax.ejb.MessageDriven;
+import javax.ejb.MessageDrivenContext;
 import javax.ejb.Stateful;
+import javax.ejb.Stateless;
+import javax.ejb.Timer;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
 
 
 /**
  *
  * @author Yannick
  */
-@Stateful
+@Stateless
 public class LoadbalancerBean implements Loadbalancer{
     
 
     
-    private AsyncResult <LoadbalancerResult> asyncresult;
+    
     private int requests;
     private Musicservice targetMusicService;
-    
-    private boolean status = false;
+    private LoadbalancerResult lbR;
+    private boolean status = true;
+    int time;
+    Future <LoadbalancerResult> asyncresult = null;;
     
     @Override
+    public LoadbalancerResult startLoadbalancerSimulation(){
+        //Starte das Loadbalancing und speichere das Ergebnis in asyncresult
+        Future <LoadbalancerResult> asyncresult = null;;
+        while(status == true){
+            asyncresult = runSimulation();
+        }
+        
+        try {
+            //Hole das Ergebnis des Loadbalancing
+            lbR = asyncresult.get();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(LoadbalancerBean.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ExecutionException ex) {
+            Logger.getLogger(LoadbalancerBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return lbR;
+    }
+    
+    
     @Asynchronous
-    public Future<LoadbalancerResult> startLoadbalancerSimulation() {
-             
-        status = true;
-        while (status){
-                 System.out.println(status);
+    public Future<LoadbalancerResult> runSimulation() {
+        requests = 10;
+        LoadbalancerResult loadbalancerResult = new LoadbalancerResultBean(new HashMap(1,requests));
+ 
+        System.out.println(status);
                  requests = (int)((Math.random()*20)+1);
                  for(int i =0; i<requests; i++){
                      try {
                          System.out.println("Rufe irgendwas auf");
                          Thread.sleep(1000);
+                         
+                         
                      } catch (InterruptedException ex) {
                          Logger.getLogger(LoadbalancerBean.class.getName()).log(Level.SEVERE, null, ex);
                      }
                  }
                  System.out.println("Anzahl der Aufrufe: " + requests);
-        }
-    
         
 
-        LoadbalancerResult loadbalancerResult = new LoadbalancerResultBean(new HashMap(1,requests));
-        asyncresult = new AsyncResult <>(loadbalancerResult);
-        return asyncresult;
+        return new AsyncResult <>(loadbalancerResult);
     }
 
     @Override
-    public LoadbalancerResult startLoadbalancerSimulationByTime() {
-        System.out.println("ich laufe parallel :)");
+    public LoadbalancerResult startLoadbalancerSimulationByTime(int time) {
+        System.out.println("Zeit:" + time);     
+        for (long stop=System.nanoTime()+TimeUnit.SECONDS.toNanos(time);stop>System.nanoTime();){
+                 System.out.println("Systemzeit:" + System.nanoTime());
+                 System.out.println("Bis:" + stop);
+                 System.out.println("Differenz:");
+                 System.out.println(System.nanoTime()-stop);
+                 try {
+                     Thread.sleep(500);
+                     System.out.println("tessssstttttt");
+                 } catch (InterruptedException ex) {
+                     Logger.getLogger(LoadbalancerBean.class.getName()).log(Level.SEVERE, null, ex);
+                 }
+                 
+             }
+             System.out.println("Zeit abgelaufen");
+        
         return null;
     }
-    @Override 
-    @Asynchronous 
+    
+    
+    @Override  
     public void stopLoadbalancerSimulation(){
-        System.out.println("stoppe Simlation");
+        asyncresult.cancel(true);
         status = false;
+        System.out.println("stoppe Simlation");
         System.out.println(status);
-  
+ 
         //Versuche zu unterbrechen
-      
-       
-        
     }
-    
-    
+ 
+  
 }
