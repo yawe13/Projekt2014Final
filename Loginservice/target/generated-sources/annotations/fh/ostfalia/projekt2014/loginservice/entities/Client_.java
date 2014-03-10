6d64659882c26c09d0bd5353e0c4a3d26180ae30
Yannick <Yannick@YannickRechner>
2014-03-10 11:31:42 +0100

    Maltes Update zum Musikdienst + LB-Zeitgesteuert

--- Loginservice/target/generated-sources/annotations/fh/ostfalia/projekt2014/loginservice/entities/Client_.java
+++ Loginservice/target/generated-sources/annotations/fh/ostfalia/projekt2014/loginservice/entities/Client_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2014-03-09T15:19:16")
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2014-03-10T11:28:33")
 @StaticMetamodel(Client.class)
 public class Client_ { 
 
